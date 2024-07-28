8669525887a48209129073e5a7e1153d506997ac
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-07-28 22:36:58 +0100

    add cart item logic

--- src/Context/shopContext.jsx
+++ src/Context/shopContext.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { createContext } from 'react';
-import all_product from '../Components/Assets/all_product';
+import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
+import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
-export default function ShopContextProvider(props){
-
-    const contextValue = {all_product};
-
-    return(
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-
+function getDefaultCart() {
+  const cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
 }
 
- 
+export default function ShopContextProvider(props) {
+  const [cartItem, setCartItem] = useState(getDefaultCart);
+
+  function addToCart(ItemId) {
+    setCartItem((prev)=>({...prev,[ItemId]:prev[ItemId]+1}))
+    console.log(cartItem);
+  }
+  function removeFromCart(ItemId) {
+    setCartItem((prev)=>({...prev,[ItemId]:prev[ItemId]-1}))
+  }
+  const contextValue = { all_product, cartItem, addToCart, removeFromCart };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+}
