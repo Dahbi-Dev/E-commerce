7452afbfdd17fc510e687b46d77b0cdffbc0d594
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-07-29 01:08:18 +0100

    add navBar media screen

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 import Logo_icon from "../Assets/logo.png";
 import Cart_icon from "../Assets/cart_icon.png";
 import { ShopContext } from "../../Context/shopContext";
+import mav_dropdown from "../Assets/nav_dropdown.png";
 
 function Navbar() {
-  const {getTotalCartItems} = useContext(ShopContext);
+  const { getTotalCartItems } = useContext(ShopContext);
   const [menu, setMenu] = useState("shop");
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+      menuRef.current.classList.toggle('nav-menu-visible')
+      e.target.classList.toggle('open')
+  };
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={Logo_icon} alt="cart_icon" />
         <p>SHOPLONG</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={mav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={() => setMenu("shop")}>
           <Link className="link" to="/">
             Shop
